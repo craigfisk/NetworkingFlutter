41962abecb6a530813b7ddd93cbaa361809101d0
Craig Fisk <craigfisk@picocosmos.net>
2020-02-29 15:31:47 -0800

    First commit on fork. Changed query.

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -2,7 +2,8 @@
 
 class Constants {
 
-    static final String HEADLINE_NEWS_URL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=insertyourapikeyhere';
+//    static final String HEADLINE_NEWS_URL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=04c245ed3319422b8d49ae0495b3f123';
+    static final String HEADLINE_NEWS_URL = 'http://newsapi.org/v2/everything?q="ListView.builder"&from=2020-02-1&sortBy=popularity&apiKey=04c245ed3319422b8d49ae0495b3f123';
 
     static final String NEWS_PLACEHOLDER_IMAGE_ASSET_URL = 'assets/placeholder.png';
 
